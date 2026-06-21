f349dbaa0b9ebc44b8dc4daec9497c13ee1f5a36
MILAN KUMAR VERMA <milansgnv22@gmail.com>
2026-06-21 23:59:19 +0530

    Added health check route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import express from "express";
 
 import airRoutes from "./routes/airRoutes.js";
 
@@ -12,7 +12,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", airRoutes);
-
+app.get("/", (req, res) => {
+  res.send("AI Air Quality Advisor Backend Running 🚀");
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
